c1cf623fbb1a79015ec02c861bb36253b39fa021
italo <italogithub@gmail.com>
2023-05-24 22:45:35 -0300

    update

--- brasileirao/test/widget_test.dart
+++ brasileirao/test/widget_test.dart
@@ -9,7 +9,6 @@ import 'package:brasileirao/App/app_home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:brasileirao/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
