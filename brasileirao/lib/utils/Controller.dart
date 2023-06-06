525ccc7e56368dfaa28df03c38fc1014207169c8
Tallys-aureliano <tallysaureliano@outlook.com>
2023-06-05 21:11:31 -0300

    :sparkles: feat: Possible new departure route

--- brasileirao/lib/utils/Controller.dart
+++ brasileirao/lib/utils/Controller.dart
@@ -3,7 +3,16 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-enum TableStatus { idle, loading, ready, readyTable, readyPlayers, readyMatches, error }
+enum TableStatus {
+  idle,
+  loading,
+  ready,
+  readyTable,
+  readyPlayers,
+  readyMatches,
+  error
+}
+
 class DataService {
   final ValueNotifier<Map<String, dynamic>> tableStateNotifier =
       ValueNotifier({'status': TableStatus.idle, 'dataObjects': []});
@@ -11,6 +20,7 @@ class DataService {
   final ValueNotifier<List<String>> propetyNamesNotifier = ValueNotifier([]);
 
   void chamarApi(index) {
+    // ignore: avoid_print
     final requisicoes = [partidas];
     tableStateNotifier.value = {
       'status': TableStatus.loading,
@@ -21,28 +31,37 @@ class DataService {
 
 // https://api.api-futebol.com.br/v1/campeonatos/10/rodadas/8
   Future<void> partidas() async {
+    // int rodadas = 8;
+    // var recPartidas = Uri(
+    //     scheme: 'https',
+    //     host: 'api.api-futebol.com.br',
+    //     path: 'v1/campeonatos/10/rodadas/$rodadas');
+    // https://api.api-futebol.com.br/v1/campeonatos/10/rodadas
     var recPartidas = Uri(
         scheme: 'https',
         host: 'api.api-futebol.com.br',
-        path: 'v1/campeonatos/10/rodadas/8');
+        path: 'v1/campeonatos/10/rodadas');
     try {
       var jsonString = await http.read(recPartidas, headers: {
-        'Authorization': 'Bearer live_b8af81619c2465683b1395abe5568b'
+        'Authorization': 'Bearer live_8450739354f3822e1db42df96806b1'
       });
-      var partidasJson = jsonDecode(jsonString)["partidas"];
+      List partidasJson = jsonDecode(jsonString);
+      var rodada = partidasJson.firstWhere((p) => p["status"] == "agendada");
+      print(rodada["rodada"]);
+      // print(rodada);
       // print(partidasJson);
       tableStateNotifier.value = {
         'status': TableStatus.readyMatches,
-        'dataObjects': partidasJson,
+        'dataObjects': [rodada],
       };
-      propetyNamesNotifier.value = ["placar"];
-      columnsNamesNotifier.value = ["Placar"];
+      propetyNamesNotifier.value = ["nome"];
+      columnsNamesNotifier.value = ["Nome"];
     } catch (e) {
+      print(e);
       tableStateNotifier.value = {
         'status': TableStatus.error,
         'dataObjects': []
       };
     }
   }
-
 }
