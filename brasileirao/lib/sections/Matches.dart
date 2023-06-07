e9741079de87b30ddfa2581c2fa705d332521a6e
Tallys-aureliano <tallysaureliano@outlook.com>
2023-06-06 21:45:28 -0300

    :sparkles: feat: Printing match cards with api data

---
+++ brasileirao/lib/sections/Matches.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class MatchesWidget extends StatelessWidget {
+  final List jsonObjects;
+  const MatchesWidget({super.key, this.jsonObjects = const []});
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: jsonObjects.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Card(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Image.network(
+                      jsonObjects[index]["time_mandante"],
+                      width: 50,
+                      height: 50,
+                      fit: BoxFit.contain,
+                    ),
+                    Text(jsonObjects[index]["sigla_mandante"]),
+                  ],
+                ),
+                Column(children: [
+                  Text(
+                    jsonObjects[index]["placar"],
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                  Text(jsonObjects[index]["data"] +
+                      " " +
+                      jsonObjects[index]["hora"])
+                ]),
+                Column(children: [
+                  Image.network(
+                    jsonObjects[index]["time_visitante"],
+                    width: 50,
+                    height: 50,
+                    fit: BoxFit.contain,
+                  ),
+                  Text(jsonObjects[index]["sigla_visitante"]),
+                ]),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
