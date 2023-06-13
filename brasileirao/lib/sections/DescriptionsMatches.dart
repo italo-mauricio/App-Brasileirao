7717d22b5ea589ce7912f73a9df71141347a890a
Tallys-aureliano <tallysaureliano@outlook.com>
2023-06-13 15:36:40 -0300

    :recycle:refactor: Refactoring json data handling and sampling

--- brasileirao/lib/sections/DescriptionsMatches.dart
+++ brasileirao/lib/sections/DescriptionsMatches.dart
@@ -4,47 +4,46 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 
 class Description extends HookWidget {
   final int id;
-  Description({super.key, required this.id});
+  final value;
+  Description({super.key, required this.id, required this.value});
 
   @override
   Widget build(BuildContext context) {
-    useEffect(() {
-      dataService.descricaoPartidas(id);
-    }, []);
 
-    return ValueListenableBuilder(
-        valueListenable: dataService.descriptionNotifier,
-        builder: (context, value, child) {
-          switch (value['status']) {
-            case DescripitonStatus.loading:
-              return const Center(
-                  child: CircularProgressIndicator(
-                color: Color.fromARGB(249, 18, 245, 10),
-              ));
-            case DescripitonStatus.ready:
-              final textsGolsM = value['data']["gols_m"]
-                  .map<Widget>((gol) => Text(
-                      "Autor do Gol: ${gol["atleta"]["nome_popular"]}\nTempo: ${gol["minuto"]}\nPeriodo: ${gol["periodo"]}"))
-                  .toList();
+    if (dataService.selectedPartidaId != id) {
+      return Text("");
+    }
 
-              final textsGolsV = value['data']["gols_v"]
-                  .map<Widget>((gol) => Text(
-                      "Autor do Gol: ${gol["atleta"]["nome_popular"]}\nTempo: ${gol["minuto"]}\nPeriodo: ${gol["periodo"]}"))
-                  .toList();
-              return Column(
-                children: [
-                  Text("${value['data']["status"]}"),
-                  Text("Local: ${value['data']["local"]}"),
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(children: textsGolsM),
-                        Column(children: textsGolsV)
-                      ]),
-                ],
-              );
-          }
-          return const Text("Erro");
-        });
+    switch (value['status']) {
+      case DescripitonStatus.idle:
+        return Text("data");
+      case DescripitonStatus.loading:
+        return const Center(
+            child: CircularProgressIndicator(
+          color: Color.fromARGB(249, 18, 245, 10),
+        ));
+      case DescripitonStatus.ready:
+        final textsGolsM = value['data']["gols_m"]
+            .map<Widget>((gol) => Text(
+                "Autor do Gol: ${gol["atleta"]["nome_popular"]}\nTempo: ${gol["minuto"]}\nPeriodo: ${gol["periodo"]}"))
+            .toList();
+
+        final textsGolsV = value['data']["gols_v"]
+            .map<Widget>((gol) => Text(
+                "Autor do Gol: ${gol["atleta"]["nome_popular"]}\nTempo: ${gol["minuto"]}\nPeriodo: ${gol["periodo"]}"))
+            .toList();
+        return Column(
+          children: [
+            Text("${value['data']["status"]}"),
+            Text("Local: ${value['data']["local"]}"),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+              Column(children: textsGolsM),
+              Column(children: textsGolsV)
+            ]),
+          ],
+        );
+    }
+    return const Text("Erro");
+
   }
 }
