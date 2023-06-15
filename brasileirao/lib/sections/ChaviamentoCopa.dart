76bfc53356775c4909f80ce0e0592b07079fbe3a
Tallys-aureliano <tallysaureliano@outlook.com>
2023-06-15 13:36:05 -0300

    :sparkles: feat: Brazilian cup matches sampling widget

--- brasileirao/lib/sections/ChaviamentoCopa.dart
+++ brasileirao/lib/sections/ChaviamentoCopa.dart
@@ -1,42 +1,93 @@
 import 'package:flutter/material.dart';
 
-class FasesCopaWidget extends StatelessWidget {
-  final Map<String, dynamic> jsonObjects;
+class PartidasCopa extends StatelessWidget {
+  final List jsonObjects;
 
-  const FasesCopaWidget({Key? key, required this.jsonObjects}) : super(key: key);
+  PartidasCopa({super.key, required this.jsonObjects});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Fases da Copa do Brasil'),
-      ),
-      body: ListView.builder(
-        itemCount: jsonObjects['fases'].length,
-        itemBuilder: (context, index) {
-          final fase = jsonObjects['fases'][index];
-          final faseNome = fase['nome'];
-          final chaves = fase['chaves'];
-
-          return ExpansionTile(
-            title: Text(faseNome),
+    return ListView.builder(
+        itemCount: jsonObjects.length,
+        
+        // shrinkWrap: true,
+        itemBuilder: (BuildContext context, int index) {
+          return Column(
             children: [
-              for (var chave in chaves) ...[
-                ListTile(
-                  title: Text('Chave ${chave['nome']}'),
-                  subtitle: Column(
-                    children: [
-                      for (var time in chave['times'])
-                        Text(time['nome_popular']),
-                    ],
-                  ),
+              // Expanded(
+              // child:
+              ListTile(
+                onTap: () => "aaaa",
+                  title: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        Image.network(jsonObjects[index]["escudo1"],
+                            width: 50, height: 50, fit: BoxFit.contain),
+                        Text(jsonObjects[index]["sigla1"])
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(jsonObjects[index]["placarIda"]),
+                        Text("Data: ${jsonObjects[index]["dataIda"]}"),
+                        Text("Hora:${jsonObjects[index]["horarioIda"]}")
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Image.network(jsonObjects[index]["escudo2"],
+                            width: 50, height: 50, fit: BoxFit.contain),
+                        Text(jsonObjects[index]["sigla2"])
+                      ],
+                    )
+                  ],
                 ),
-                Divider(),
-              ],
+                // ),
+              )),
+              ListTile(
+                onTap: () => "aaaa",
+                  title: Padding(
+                padding: const EdgeInsets.all(0.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        Image.network(jsonObjects[index]["escudo2"],
+                            width: 50, height: 50, fit: BoxFit.contain),
+                        Text(jsonObjects[index]["sigla2"])
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(jsonObjects[index]["placarVolta"]),
+                        Text("Data: ${jsonObjects[index]["dataVolta"]}"),
+                        Text("Hora:${jsonObjects[index]["horarioVolta"]}")
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Image.network(jsonObjects[index]["escudo1"],
+                            width: 50, height: 50, fit: BoxFit.contain),
+                        Text(jsonObjects[index]["sigla1"])
+                      ],
+                    )
+                  ],
+                ),
+              )),
+              const Divider(
+                height: 20,
+                color: Colors.green,
+                thickness: 1,
+                indent: 20,
+                endIndent: 20,
+              )
             ],
           );
-        },
-      ),
-    );
+        });
   }
 }
