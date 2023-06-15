dec3d9d834be479ff26471b90d58c2d17475bd61
Tallys-aureliano <tallysaureliano@outlook.com>
2023-06-15 15:00:45 -0300

    :sparkles:feat:Adding subtitle with widget of description of match

--- brasileirao/lib/sections/ChaviamentoCopa.dart
+++ brasileirao/lib/sections/ChaviamentoCopa.dart
@@ -1,4 +1,7 @@
+import 'package:brasileirao/controllers/BrasileiraoController.dart';
 import 'package:flutter/material.dart';
+
+import 'DescriptionsMatches.dart';
 
 class PartidasCopa extends StatelessWidget {
   final List jsonObjects;
@@ -9,76 +12,88 @@ class PartidasCopa extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
         itemCount: jsonObjects.length,
-        
-        // shrinkWrap: true,
         itemBuilder: (BuildContext context, int index) {
+          final idIda = jsonObjects[index]["idIda"];
+          final idVolta = jsonObjects[index]["idVolta"];
           return Column(
             children: [
-              // Expanded(
-              // child:
               ListTile(
-                onTap: () => "aaaa",
+                  onTap: () {
+                    dataService.descricaoPartidas(idIda);
+                  },
                   title: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Image.network(jsonObjects[index]["escudo1"],
-                            width: 50, height: 50, fit: BoxFit.contain),
-                        Text(jsonObjects[index]["sigla1"])
+                        Column(
+                          children: [
+                            Image.network(jsonObjects[index]["escudo1"],
+                                width: 50, height: 50, fit: BoxFit.contain),
+                            Text(jsonObjects[index]["sigla1"])
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Text(jsonObjects[index]["placarIda"]),
+                            Text("Data: ${jsonObjects[index]["dataIda"]}"),
+                            Text("Hora:${jsonObjects[index]["horarioIda"]}")
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Image.network(jsonObjects[index]["escudo2"],
+                                width: 50, height: 50, fit: BoxFit.contain),
+                            Text(jsonObjects[index]["sigla2"])
+                          ],
+                        )
                       ],
                     ),
-                    Column(
-                      children: [
-                        Text(jsonObjects[index]["placarIda"]),
-                        Text("Data: ${jsonObjects[index]["dataIda"]}"),
-                        Text("Hora:${jsonObjects[index]["horarioIda"]}")
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Image.network(jsonObjects[index]["escudo2"],
-                            width: 50, height: 50, fit: BoxFit.contain),
-                        Text(jsonObjects[index]["sigla2"])
-                      ],
-                    )
-                  ],
-                ),
-                // ),
-              )),
+                    // ),
+                  ),
+                  subtitle: ValueListenableBuilder(
+              valueListenable: dataService.descriptionNotifier,
+              builder: (context, value, child) =>
+                  Description(id: idIda, value: value)),
+                  ),
               ListTile(
-                onTap: () => "aaaa",
+                  onTap: () {
+                    dataService.descricaoPartidas(idVolta);
+                  },
                   title: Padding(
-                padding: const EdgeInsets.all(0.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
+                    padding: const EdgeInsets.all(0.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Image.network(jsonObjects[index]["escudo2"],
-                            width: 50, height: 50, fit: BoxFit.contain),
-                        Text(jsonObjects[index]["sigla2"])
+                        Column(
+                          children: [
+                            Image.network(jsonObjects[index]["escudo2"],
+                                width: 50, height: 50, fit: BoxFit.contain),
+                            Text(jsonObjects[index]["sigla2"])
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Text(jsonObjects[index]["placarVolta"]),
+                            Text("Data: ${jsonObjects[index]["dataVolta"]}"),
+                            Text("Hora:${jsonObjects[index]["horarioVolta"]}")
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Image.network(jsonObjects[index]["escudo1"],
+                                width: 50, height: 50, fit: BoxFit.contain),
+                            Text(jsonObjects[index]["sigla1"])
+                          ],
+                        )
                       ],
                     ),
-                    Column(
-                      children: [
-                        Text(jsonObjects[index]["placarVolta"]),
-                        Text("Data: ${jsonObjects[index]["dataVolta"]}"),
-                        Text("Hora:${jsonObjects[index]["horarioVolta"]}")
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Image.network(jsonObjects[index]["escudo1"],
-                            width: 50, height: 50, fit: BoxFit.contain),
-                        Text(jsonObjects[index]["sigla1"])
-                      ],
-                    )
-                  ],
-                ),
-              )),
+                  ),
+                  subtitle: ValueListenableBuilder(
+              valueListenable: dataService.descriptionNotifier,
+              builder: (context, value, child) =>
+                  Description(id: idVolta, value: value)),
+                  ),
               const Divider(
                 height: 20,
                 color: Colors.green,
